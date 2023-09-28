69966ac740a5c5d9e6bd5ca230f62380f67206c9
Alex <sanya.vimeworld@gmail.com>
2023-09-28 21:52:34 +0600

    #1

--- Program.cs
+++ Program.cs
@@ -4,11 +4,11 @@
 System.Console.WriteLine($"{msg}");
 return Convert.ToInt32(Console.ReadLine());
 }
-int number = Prompt("Введите трехзначное число");
+int number = Prompt("Введите трехзначное число:");
 if (number >=100 && number <1000)
 {
 int mid = (number % 100 - number % 10) / 10;
 System.Console.WriteLine($"Вторая цифра числа {number} это {mid}");
 }
 else
-System.Console.WriteLine("Третьей цифры нет!");
+System.Console.WriteLine("Третьей цифры нет:");
